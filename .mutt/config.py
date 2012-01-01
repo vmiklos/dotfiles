981cc546c2c22d249ba872388d7a4e8d6551d25b
Miklos Vajna <vmiklos@frugalware.org>
2012-01-01 20:32:27 +0100

    openscope--

--- .mutt/config.py
+++ .mutt/config.py
@@ -1,8 +1,7 @@
 import glob, os
 
 ignore = {
-		"frugalware.org": ["INBOX.Lists.openscope",
-			"INBOX.Lists.qemu-devel", "INBOX.Lists.qbitlbee", "INBOX.Lists.ga-swig", "INBOX.Lists.pootle", "INBOX.Lists.frugalware-security-vs",
+		"frugalware.org": ["INBOX.Lists.qemu-devel", "INBOX.Lists.qbitlbee", "INBOX.Lists.ga-swig", "INBOX.Lists.pootle", "INBOX.Lists.frugalware-security-vs",
 			"INBOX.Lists.frugalware-users-hu", "INBOX.Lists.frugalware-forums",
 			"INBOX.Lists.virt-tools", "INBOX.Lists.mplayer-dev-eng", "INBOX.Lists.pacman-dev", "INBOX.Lists.gsoc"]
 		}
