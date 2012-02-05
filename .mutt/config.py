331df93f7e6ee4dfda38158e1e5bd40e702b13b9
Miklos Vajna <vmiklos@frugalware.org>
2012-02-05 13:47:56 +0100

    properly unsub from swig

--- .mutt/config.py
+++ .mutt/config.py
@@ -1,7 +1,7 @@
 import glob, os
 
 ignore = {
-		"frugalware.org": ["INBOX.Lists.ga-swig", "INBOX.Lists.pootle", "INBOX.Lists.frugalware-security-vs",
+		"frugalware.org": ["INBOX.Lists.pootle", "INBOX.Lists.frugalware-security-vs",
 			"INBOX.Lists.frugalware-users-hu", "INBOX.Lists.frugalware-forums",
 			"INBOX.Lists.virt-tools", "INBOX.Lists.mplayer-dev-eng", "INBOX.Lists.pacman-dev", "INBOX.Lists.gsoc"]
 		}
