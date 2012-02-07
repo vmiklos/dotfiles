0461ae80a49f3a0d87bd776a99458ff25295ada7
Miklos Vajna <vmiklos@frugalware.org>
2012-02-07 23:40:19 +0100

    all gsoc mail is archived now

--- .mutt/config.py
+++ .mutt/config.py
@@ -3,5 +3,5 @@ import glob, os
 ignore = {
 		"frugalware.org": ["INBOX.Lists.frugalware-security-vs",
 			"INBOX.Lists.frugalware-users-hu", "INBOX.Lists.frugalware-forums",
-			"INBOX.Lists.virt-tools", "INBOX.Lists.mplayer-dev-eng", "INBOX.Lists.pacman-dev", "INBOX.Lists.gsoc"]
+			"INBOX.Lists.virt-tools", "INBOX.Lists.mplayer-dev-eng", "INBOX.Lists.pacman-dev"]
 		}
