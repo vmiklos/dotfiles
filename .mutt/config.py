81b2862b30eb56f68d501da5dd3e3aa096fb2615
Miklos Vajna <vmiklos@frugalware.org>
2012-03-04 12:39:59 +0100

    v-s is dead

--- .mutt/config.py
+++ .mutt/config.py
@@ -1,7 +1,4 @@
-import glob, os
-
 ignore = {
-		"frugalware.org": ["INBOX.Lists.frugalware-security-vs",
-			"INBOX.Lists.frugalware-users-hu", "INBOX.Lists.frugalware-forums",
+		"frugalware.org": ["INBOX.Lists.frugalware-users-hu", "INBOX.Lists.frugalware-forums",
 			"INBOX.Lists.mplayer-dev-eng", "INBOX.Lists.pacman-dev"]
 		}
