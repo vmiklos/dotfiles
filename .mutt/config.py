9fe0ba7905c81965b4f00d3839db4353fcd197dc
Miklos Vajna <vmiklos@frugalware.org>
2012-03-08 21:49:45 +0100

    mutt: no need to ignore folders anymore

--- .mutt/config.py
+++ .mutt/config.py
@@ -1,4 +1,3 @@
 ignore = {
-		"frugalware.org": ["INBOX.Lists.frugalware-users-hu", "INBOX.Lists.frugalware-forums",
-			"INBOX.Lists.mplayer-dev-eng", "INBOX.Lists.pacman-dev"]
+		"example.com": ["INBOX.Lists.foobar"]
 		}
