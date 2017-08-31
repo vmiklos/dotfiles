17f01dd4ec376cec7e546fabb6b52fe799d65173
Miklos Vajna <vmiklos@collabora.co.uk>
2017-08-31 16:42:55 +0200

    imapfilter, vim: update configs

--- .imapfilter/config.lua
+++ .imapfilter/config.lua
@@ -3,9 +3,11 @@
 account = IMAP {
 	server = 'mail.collabora.co.uk',
 	username = 'vmiklos',
-	ssl = 'tls1'
+	ssl = 'yes'
 }
 results = account['INBOX']:is_older(90)
 results:move_messages(account['ZArchive'])
 results = account['Lists.libreoffice']:is_older(90)
+results:delete_messages()
+results = account['Lists.libreoffice-bugs']:is_older(90)
 results:delete_messages()
