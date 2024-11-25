bb1afda58165cdeb18bb4a2565ee319ca9373fd3
Miklos Vajna <vmiklos@collabora.com>
2024-11-25 15:17:12 +0100

    imapfilter: adapt to zoho

--- .imapfilter/config.lua
+++ .imapfilter/config.lua
@@ -1,17 +1,17 @@
 -- gmail: echo "before:$(date --date="90 days ago" +%Y/%m/%d) label:lists-libreoffice"
 
 account = IMAP {
-	server = 'mail.collabora.com',
-	username = 'vmiklos',
+	server = 'imappro.zoho.com',
+	username = 'miklos.vajna@collabora.com',
 	ssl = 'yes'
 }
 results = account['INBOX']:is_older(90)
 results:move_messages(account['ZArchive'])
-results = account['Lists.libreoffice']:is_older(90)
+results = account['Lists/libreoffice']:is_older(90)
 results:delete_messages()
-results = account['Lists.libreoffice-bugs']:is_older(90)
+results = account['Lists/libreoffice-bugs']:is_older(90)
 results:delete_messages()
-results = account['Lists.cp-marketing']:is_older(90)
+results = account['Lists/cp-marketing']:is_older(90)
 results:delete_messages()
 results = account['Spam']:is_older(90)
 results:delete_messages()
